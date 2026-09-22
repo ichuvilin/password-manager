bcaa7137741766eaf1297a3a52a5ecc0ebf8ab39
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-22 13:28:25 +0500

    create password struct

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "time"
+
+type Password struct {
+	Name         string    `json:"name"`
+	Value        string    `json:"value"`
+	Category     string    `json:"category"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastModified time.Time `json:"last_modified"`
+}
+
+func NewPassword(name, value, category string) Password {
+	return Password{Name: name, Value: value, Category: category, CreatedAt: time.Now(), LastModified: time.Now()}
+}
+
 func main() {
 
 }
