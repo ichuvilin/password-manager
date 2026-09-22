14b30d5b84188391d41e75e0ff935ef05aa853f5
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-22 13:32:51 +0500

    create PasswordManager

--- main.go
+++ main.go
@@ -10,6 +10,21 @@ type Password struct {
 	LastModified time.Time `json:"last_modified"`
 }
 
+type PasswordManager struct {
+	passwords    map[string]Password `json:"passwords"`
+	masterKey    []byte              `json:"masterKey"`
+	filePath     string              `json:"file_path"`
+	isInitialize bool                `json:"-"`
+}
+
+func NewPasswordManage(filePath string) *PasswordManager {
+	return &PasswordManager{
+		passwords:    make(map[string]Password),
+		filePath:     filePath,
+		isInitialize: false,
+	}
+}
+
 func NewPassword(name, value, category string) Password {
 	return Password{Name: name, Value: value, Category: category, CreatedAt: time.Now(), LastModified: time.Now()}
 }
